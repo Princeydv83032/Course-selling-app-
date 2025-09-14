a0277c8f4e0d640bb96c30bdd553680e76637a77
Prince Yadav <pyadav15734@gmail.com>
2025-09-15 04:11:16 +0530

    added backend code + .gitignore

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const DB_URI = process.env.MONGO_URL;
+
+try {
+  await mongoose.connect(DB_URI);
+  console.log("connected to MongoDB");
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
